75d942ddef84e9c9f0d560d32082862d32fa498f
Leo Allen <leo.f.allen@gmail.com>
2013-04-02 14:45:32 +0100

    Use hashes rather than loads of separate constants

--- monopoly/lib/station.rb
+++ monopoly/lib/station.rb
@@ -2,10 +2,12 @@ class Station < Land
 
   PURCHASE_VALUE = 200
   RENT_VALUE = 50
-  RENT_VALUE_ONE_OWNED = RENT_VALUE
-  RENT_VALUE_TWO_OWNED = 100
-  RENT_VALUE_THREE_OWNED = 200
-  RENT_VALUE_FOUR_OWNED = 400
+  RENT_VALUES = {
+    1 => RENT_VALUE,
+    2 => 100,
+    3 => 200,
+    4 => 400
+  }
 
   def initialize(name, group)
     super(name, PURCHASE_VALUE, RENT_VALUE, group)
@@ -13,12 +15,7 @@ class Station < Land
 
   def calculate_rent
     if @owner
-      case @group.land_owned_by(@owner).count
-        when 2 then RENT_VALUE_TWO_OWNED
-        when 3 then RENT_VALUE_THREE_OWNED
-        when 4 then RENT_VALUE_FOUR_OWNED
-        else super
-      end
+      RENT_VALUES[@group.land_owned_by(@owner).count] || super
     else
       super
     end
